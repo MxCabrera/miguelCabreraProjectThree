0e5b71b5ddfce1f32d28d73256feeed68f2d7fd2
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-04-01 17:57:36 -0400

    connected html elements to script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log(`it works!`);
+
+let score = 0;
+
+const quizBox = document.querySelector('.quizBox');
+console.log(quizBox)
+const questionMain = document.querySelector('.question');
+console.log(questionMain);
+const choice1 = document.querySelector('.choice1');
+console.log(choice1);
+const choice2 = document.querySelector('.choice2');
+console.log(choice2);
+const nextButton = document.querySelector('.next');
+console.log(nextButton);
+const previousButton = document.querySelector('.previous');
+console.log(previousButton);
