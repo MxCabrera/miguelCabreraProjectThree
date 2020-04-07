178a3b9fa261715affd685da3e1a743eb714ecc1
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-04-07 18:21:53 -0400

    completed object in an object in an array

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 // questions in an array 
+const playerInfo = {};
 const questions = [
     {
         "question": "What is your favourite food?",
@@ -85,6 +86,96 @@ const questions = [
     //     "answer4Total": "4"
     // }
 ];
+
+
+
+    playerInfo.james = {
+        name: 'LeBron James',
+        image: './assets/leBronJames.jpg',
+        class: 'lebronJ',
+        awards: `6x NBA Champion | 14x AllStar | 5x MVP | Rookie of the Year | 6x Finals MVP | 10x Scoring Champ | 3x Steals Champ | 11x Defensive 1st Team`,
+        info: 'LeBron James is known for his basketball IQ, his strength, his out of world body that maintains him for 17 straight season of basketball. Widely known as King James he has been in the finals in 8 out of 9 of the last Finals.',
+        stats: {
+            points: 27.4,
+            rebounds: 7.4,
+            assists: 7.4,
+            steals: 0.7,
+            blocks: 0.8,
+            fieldGoal: 50
+        }
+    };
+
+    playerInfo.jordan = {
+        name: 'Michael Jordan',
+        image: './assets/michaelJordan.jpg',
+        class: 'mJordan',
+        awards: `6x NBA Champion | 14x AllStar | 5x MVP | Rookie of the Year | 6x Finals MVP | 10x Scoring Champ | 3x Steals Champ | 11x Defensive 1st Team`,
+        info: 'Michael Jordan known as the greatest basketball player ever to play the game. He won a championship 6 times, but that feat only gets better when you hear that he won 3 straight championships; twice! He retired from basketball to play baseball after the first 3-peat, only to come back and win another 3 championships.',
+        stats: {
+            points: 30.0,
+            rebounds: 6.2,
+            assists: 5.3,
+            steals: 2.3,
+            blocks: 0.8,
+            fieldGoal: 49
+        }
+    };
+
+    playerInfo.magic = {
+        name: 'Magic Johnson',
+        image: './assets/magicJohnson2.jpeg',
+        class: 'magic',
+        awards: `5x NBA Champion | 12x AllStar | 3x MVP | 2x Steal Champ | 3x Finals MVP | 4x Assist Champ`,
+        info: `Magic Johnson known for his passing ability and height advantage over his opponents. As a point guard for the Los Angeles Lakers, he bolsters an incredibly 6'9" frame to match his IQ.`,
+        stats: {
+            points: 19.5,
+            rebounds: 7.2,
+            assists: 11.2,
+            steals: 0.8,
+            blocks: 0.4,
+            fieldGoal: 52
+        }
+    };
+
+    playerInfo.duncan = {
+        name: 'Tim Duncan',
+        image: './assets/timDuncan.jpg',
+        class: 'timothy',
+        awards: `5x NBA Champion | 15x AllStar | 2x MVP | Rookie of the Year | 3x Finals MVP`,
+        info: `Tim Duncan known for his fundamental game. He knew how to maneuver around the basket with high IQ and veteran moves wihtout exhausting too much energy.`,
+        stats: {
+            points: 19.0,
+            rebounds: 10.8,
+            assists: 3.0,
+            steals: 0.7,
+            blocks: 2.2,
+            fieldGoal: 50
+        }
+    };
+
+
+
+// const gifs = [
+//     {
+//         link: 'https://media.giphy.com/media/dH16CNQJRwPsY/giphy.gif',
+//         tag: 'jordan',
+//     },
+//     {
+//         link: 'https://media.giphy.com/media/XkJlbLt9fbDBS/giphy.gif',
+//         tag: 'lebron',
+//     },
+//     {
+//         link: 'https://media.giphy.com/media/gcXcSRYZ9cGWY/giphy.gif',
+//         tag: 'magic',
+//     },
+//     {
+//         link: 'https://media.giphy.com/media/gcXcSRYZ9cGWY/giphy.gif',
+//         tag: 'timmy',
+//     }];
+// console.log(`the user selection is ${selection}`);
+// Get value from weather object based on user selection
+// const dailyWeather = weather[selection];
+// const lowerWeather = dailyWeather.toLowerCase()
 // console.log(questions.answer1)
 
 let score = [];
@@ -113,7 +204,7 @@ function beginQuiz() {
         header.style.display = 'none';
         quiz.style.display = 'block';
     }
-}
+};
 
 
 //.. create a question when user enters the DOM
@@ -124,17 +215,17 @@ function createQuestions(index){
     const choice3Total = questions[index].answer3Total;
     const choice4Total = questions[index].answer4Total;
 
-    questionMain.innerHTML = `${index + 1}. ${question.question}`
+    questionMain.innerHTML = `${index + 1}. ${question.question}`;
     
     choice1.setAttribute('data-total', `${choice1Total}`);
     choice2.setAttribute('data-total', `${choice2Total}`);
     choice3.setAttribute('data-total', `${choice3Total}`);
     choice4.setAttribute('data-total', `${choice4Total}`);
 
-    choice1.innerHTML = `${question.answer1}`
-    choice2.innerHTML = `${question.answer2}`
-    choice3.innerHTML = `${question.answer3}`
-    choice4.innerHTML = `${question.answer4}`
+    choice1.innerHTML = `${question.answer1}`;
+    choice2.innerHTML = `${question.answer2}`;
+    choice3.innerHTML = `${question.answer3}`;
+    choice4.innerHTML = `${question.answer4}`;
 
     // use the value of answer.......
 }
@@ -169,70 +260,60 @@ function nextQuestion(){
         // header.style.display = 'block';
         quiz.style.display = 'none';
 
-        // const pName = null;
-        // const pImage = null ;
-        // const pInfo = null;
-        // if (finalScore >= 5 && finalScore <= 7) {
-        //     pName = `Michael Jordan`;
-        //     pImage = `<img src="./assets/michaelJordan.jpg" alt="michael jordan">`;
-        //     pInfo = `6x NBA Champion`
-        // } else if (finalScore >= 8 && finalScore <= 10) {
-        //     pName = `LeBron James`;
-        //     pImage = `<img src="./assets/leBronJames.jpg" alt="lebron james">`;
-        //     pInfo = `3x NBA Champion`
-        //     // return;
-        // }
 
-        // results.innerHTML = `
-        // <div class="resultBox">
-        // ${image}
-        // <h1>You are ${name}!</h1>
-        // <p>${info}</p>
-        // </div>
-        // <button class="restart"> Restart </button>
-        // `;
-
-
+        let pName = '';
+        let pImage = '';
+        let pImageClass = '';
+        let pAwards = '';
+        let pInfo = '';
+        let pStats = '';
 
         if (finalScore >= 5 && finalScore <= 9) {
-            results.innerHTML = `
-            <h1> Your Score: ${finalScore}</h1>
-            <div class="resultBox">
-            <img src="./assets/leBronJames.jpg" alt="Lebron James">
-            <h2>You are Lebron James!</h2>
-            <p>3x NBA Champion</p>
-            </div>
-            <button class="restart"> Restart </button>`
+            pName = `${playerInfo.james.name}`;
+            pImage = `${playerInfo.james.image}`;
+            pImageClass = `${playerInfo.james.class}`;
+            pAwards = `${playerInfo.james.awards}`;
+            pInfo = `${playerInfo.james.info}`;
+            pStats = `Points: ${playerInfo.james.stats.points} | Rebounds: ${playerInfo.james.stats.rebounds} | Assists: ${playerInfo.james.stats.assists} | Steals: ${playerInfo.james.stats.steals} | Blocks: ${playerInfo.james.stats.blocks} | Field Goal: ${playerInfo.james.stats.fieldGoal}%`
         } else if (finalScore >= 10 && finalScore <= 13) {
-            results.innerHTML = `
-            <h1> Your Score: ${finalScore}</h1>
-            <div class="resultBox">
-            <img src="./assets/michaelJordan.jpg" alt="Michael Jordan">
-            <h2>You are Michael Jordan!</h2>
-            <p>6x NBA Champion</p>
-            </div>
-            <button class="restart"> Restart </button>`
+            pName = `${playerInfo.jordan.name}`;
+            pImage = `${playerInfo.jordan.image}`;
+            pImageClass = `${playerInfo.jordan.class}`;
+            pAwards = `${playerInfo.jordan.awards}`;
+            pInfo = `${playerInfo.jordan.info}`;
+            pStats = `Points: ${playerInfo.jordan.stats.points} | Rebounds: ${playerInfo.jordan.stats.rebounds} | Assists: ${playerInfo.jordan.stats.assists} | Steals: ${playerInfo.jordan.stats.steals} | Blocks: ${playerInfo.jordan.stats.blocks} | Field Goal: ${playerInfo.jordan.stats.fieldGoal}%`
         } else if (finalScore >= 14 && finalScore <= 16){
-            results.innerHTML = `
-            <h1 class="magicHeader"> Your Score: ${finalScore}</h1>
-            <div class="resultBox">
-            <img class="magic" src="./assets/magicJohnson2.jpeg" alt="Magic Johnson">
-            <h2>You are Magic Johnson!</h2>
-            <p>5x NBA Champion</p>
-            </div>
-            <button class="restart"> Restart </button>`
+            pName = `${playerInfo.magic.name}`;
+            pImage = `${playerInfo.magic.image}`;
+            pImageClass = `${playerInfo.magic.class}`;
+            pAwards = `${playerInfo.magic.awards}`;
+            pInfo = `${playerInfo.magic.info}`;
+            pStats = `Points: ${playerInfo.magic.stats.points} | Rebounds: ${playerInfo.magic.stats.rebounds} | Assists: ${playerInfo.magic.stats.assists} | Steals: ${playerInfo.magic.stats.steals} | Blocks: ${playerInfo.magic.stats.blocks} | Field Goal: ${playerInfo.magic.stats.fieldGoal}%`
         } else if (finalScore >= 17 && finalScore <= 20) {
-            results.innerHTML = `
+            pName = `${playerInfo.duncan.name}`;
+            pImage = `${playerInfo.duncan.image}`;
+            pImageClass = `${playerInfo.duncan.class}`;
+            pAwards = `${playerInfo.duncan.awards}`;
+            pInfo = `${playerInfo.duncan.info}`;
+            pStats = `Points: ${playerInfo.duncan.stats.points} | Rebounds: ${playerInfo.duncan.stats.rebounds} | Assists: ${playerInfo.duncan.stats.assists} | Steals: ${playerInfo.duncan.stats.steals} | Blocks: ${playerInfo.duncan.stats.blocks} | Field Goal: ${playerInfo.duncan.stats.fieldGoal}%`
+        }
+
+
+        results.innerHTML = `
             <h1> Your Score: ${finalScore}</h1>
             <div class="resultBox">
-            <img class="timothy" src="./assets/timDuncan.jpg" alt="Tim Duncan">
-            <h2>You are Tim Duncan!</h2>
-            <p>5x NBA Champion</p>
+            <img class="${pImageClass}" src="${pImage}">
+            <h2>You are ${pName}!</h2>
+            <p>${pAwards}</p>
+            <p>${pInfo}</p>
+            <h3>Career Statistics: (Per Game Average)</h3>
+            <p>${pStats}</p>
             </div>
             <button class="restart"> Restart </button>`
-        }
+            // console.log(pInfo)
         //another if statement for multiple choices.
     }
+
 
 createQuestions(mainQuestion);
 }
@@ -263,6 +344,8 @@ function previousQuestion() {
 
 
 // onclick event listeners
+// $('results').on('click', function (event) {
+// const results = document.querySelectocr('.results');
 results.addEventListener('click', resetQuiz);
 previousButton.addEventListener('click', previousQuestion);
 nextButton.addEventListener('click', nextQuestion);
